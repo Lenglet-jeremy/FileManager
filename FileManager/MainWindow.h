cc857b606d73bb84260f15c92147f8ad78279bec
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2023-11-22 21:36:52 +0100

    Add files via upload

---
+++ FileManager/MainWindow.h
@@ -0,0 +1,106 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QSizeGrip>
+#include <QTimer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void varInitialize();
+    void setTitleBar();
+    void setMenuBar();
+    void setStatusBar();
+    void setLayout();
+
+protected :
+    bool eventFilter(QObject *object, QEvent *event) override;
+    void mousePressEvent(QMouseEvent *) override;
+    void mouseMoveEvent(QMouseEvent *) override;
+    void mouseReleaseEvent(QMouseEvent *) override;
+    void resizeEvent(QResizeEvent * Event) override;
+
+private slots:
+
+    void onClicked();
+
+
+private:
+
+    int XPos;
+    int YPos;
+
+    int globalX;
+    int globalY;
+
+    int startTopBorder;
+    int startRightBorder;
+    int startBottomBorder;
+    int startLeftBorder;
+
+    int currentTopBorder;
+    int currentRightBorder;
+    int currentBottomBorder;
+    int currentLeftBorder;
+
+    bool onTopLeft;
+    bool onTopRight;
+    bool onBottomLeft;
+    bool onBottomRight;
+    bool onTop;
+    bool onRight;
+    bool onBottom;
+    bool onLeft;
+
+    bool topLeftGrabbed;
+    bool topRightGrabbed;
+    bool bottomLeftGrabbed;
+    bool bottomRightGrabbed;
+    bool topGrabbed;
+    bool rightGrabbed;
+    bool bottomGrabbed;
+    bool leftGrabbed;
+
+    bool isPressed;
+
+    QWidget * TitleBar;
+    QLabel * Icon;
+    QLabel * Title;
+    QPushButton * Minimize;
+    QPushButton * Maximize;
+    QPushButton * Close;
+    QHBoxLayout * TitleBarLayout;
+
+    QMenuBar * MenuBar;
+    QMenu * FileMenu;
+    QMenu * NewAction;
+    QAction * NewTerminal;
+    QAction * NewWindow;
+    QMenu * LoadAction;
+    QMenu * SaveAction;
+    QAction * Save;
+    QAction * SaveAs;
+
+    QStatusBar * StatusBar;
+    QSizeGrip * SizeGrip;
+
+
+    QWidget * CentralWidget;
+
+
+    QVBoxLayout * VLayout;
+};
+#endif // MAINWINDOW_H
